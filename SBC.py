e18ccd2b5b005ac6193cc7306485d39ae2a792f2
rjtsg <rjtsg@users.noreply.github.com>
2019-01-23 15:20:47 +0100

    Added calculations upto high pressure turbine

--- SBC.py
+++ SBC.py
@@ -42,10 +42,25 @@ mcore = m1/(BPR+1)
 Pt13 = FPR*Pt2
 Pt21 = Pt13
 Tt13 = Tt2*(1 + 1/n_fie*((Pt13/Pt2)**((kair-1)/kair)-1))
+Tt21 = Tt13
 m21 = mcore
 m13 = mbp
 
 ## LPC ##
+Pt25 = PR_LPC*Pt21
+Tt25 = Tt21*(1 + 1/n_fie*((Pt25/Pt21)**((kair-1)/kair)-1))
+m25 = mcore
+
+## HPC ##
+Pt3 = PR_HPC*Pt25
+Tt3 = Tt25*(1 + 1/n_fie*((Pt3/Pt25)**((kair-1)/kair)-1))
+m3 = mcore
+
+## Combustion chamber ##
+Tt4 = T4
+mf = m3*Cpgas*(Tt4 - Tt3)/(n_ce*LHV)
+m4 = m3 + mf
+Pt4 = cpr*Pt3
 
 ##Print Values##
 print('-'*40)
@@ -60,5 +75,19 @@ print('Pt13 = Pt21 = %.2f Pa' % Pt13)
 print('Tt13 = Tt21 = %.2f K' % Tt13)
 print('m21 = %.2f kg/s and m13 = %.2f kg/s'%(m21,m13))
 print('*'*10 + 'LPC calculations' + '*'*10)
+print('Pt25 %.2f Pa' % Pt25)
+print('Tt25 %.2f K' % Tt25)
+print('m25 = %.2f kg/s' % m25)
+print('*'*10 + 'HPC calculations' + '*'*10)
+print('Pt3 %.2f Pa' % Pt3)
+print('Tt3 %.2f K' % Tt3)
+print('m3 = %.2f kg/s' % m3)
+print('*'*10 + 'Combustion chamber calculations' + '*'*10)
+print('Pt4 = %.2f Pa' % Pt4)
+print('Tt4 = %.2f K' % Tt4)
+print('mf = %.2f kg/s and m4 = %.2f kg/s' %(mf,m4))
+
+### exit the program
+input('Press ENTER to exit')
 
 
